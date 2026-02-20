10e19d853269226dd13f5f52e22d1f22a34391a7
Sumit993905 <officialsumitraj993905@gmail.com>
2026-02-20 10:58:58 +0530

    done chnages

---
+++ Chat_AC_VCApp/Views/Video/VideoCallView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoCallView.swift
+//  Chat_AC_VCApp
+//
+//  Created by Sumit Raj Chingari on 18/02/26.
+//
+
+import SwiftUI
+
+struct VideoCallView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoCallView()
+}
